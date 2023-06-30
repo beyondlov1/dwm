2a435d7a98b040cbcc0590612fe52cca7e74ce73
beyond <beyondlov1@hotmail.com>
2023-06-30 11:05:47 +0800

    config

--- config.def.h
+++ config.def.h
@@ -161,7 +161,7 @@ static const char *todotxtcmd[] = {"st","-e","python3", "/home/beyond/software/b
 static const char *enotecmd[] = {"st","-e","python3", "/home/beyond/software/bin/enotepy/app.py",NULL};
 static const char *copyqcmd[] = {"copyq", "toggle", NULL };
 //static const char *browsercmd[] = {"firefox", NULL};
-static const char *browsercmd[] = {"ba", NULL};
+static const char *browsercmd[] = {"/home/beyond/software/ba", NULL};
 
 static const TaskGroup taskgroup1 = {
 	2,
