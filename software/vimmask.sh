5b597e98a2f1d6329e20497de85474f38901042e
beyondlov1 <beyondlov1@hotmail.com>
2024-01-27 14:09:31 +0800

    add

--- software/vimmask.sh
+++ software/vimmask.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 check=$(wmctrl -l | grep  N/A)
 if [ x"$check" == x"" ];then
-  sudo vimmask event7
+  sudo vimmask $(cat /proc/bus/input/devices | grep -A10 Mouse | grep event | awk -F"=" '{print $2}' | grep -Eo "event[0-9]+" | head -n 1)
 fi
