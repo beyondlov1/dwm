5acc4b51c589f14bb9fed0ec522f4b8a865706a7
beyondlov1 <beyondlov1@hotmail.com>
2024-03-01 15:31:01 +0800

    add

--- config.def.h
+++ config.def.h
@@ -171,7 +171,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-l", "10", NULL };
-static const char *rofiruncmd[] = { "rofi", "-show", "run", NULL};
+static const char *rofiruncmd[] = { "rofi", "-show", "run", "-matching", "fuzzy", NULL};
 //static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 // static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
