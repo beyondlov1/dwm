b61c6e7d6c697bbc330f26ac00284ea0cc826482
beyondlov1 <beyondlov1@hotmail.com>
2024-08-26 20:16:36 +0800

    add

--- software/smartclose.sh
+++ software/smartclose.sh
@@ -2,10 +2,12 @@
 role=$(xprop -id $(xdotool getwindowfocus getwindowgeometry --shell | grep WINDOW | awk -F= '{print $2}') | grep WM_WINDOW_ROLE | awk -F\" '{print $2}')
 event=$(cat /proc/bus/input/devices | grep -A10 Keyboard | grep event | awk -F"=" '{print $2}' | grep -Eo "event[0-9]+" | head -n 1)
 if [[ "$role" == "browser" || "$role" == "browser-window" ]]; then
-	sudo simulate_key $event 2 29 17
+	xdotool key "Ctrl+w"
+	# sudo simulate_key $event 2 29 17
 else
-	sudo simulate_key $event 21 59
-	sudo simulate_key $event 21 56
-	sudo simulate_key $event 2 56 62
+	xdotool key "Super+F1"
+	# sudo simulate_key $event 21 59
+	# sudo simulate_key $event 21 56
+	# sudo simulate_key $event 2 56 62
 fi
 
--- software/smartleft.sh
+++ software/smartleft.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 role=$(xprop -id $(xdotool getwindowfocus getwindowgeometry --shell | grep WINDOW | awk -F= '{print $2}') | grep WM_WINDOW_ROLE | awk -F\" '{print $2}')
-class=$(hyprctl -j activewindow | jq -r ".class")
+# class=$(hyprctl -j activewindow | jq -r ".class")
 if [[ "$role" == "browser" || "$class" == "firefox" ]]; then
-	sudo /home/beyond/c-project/keyboard/simulate_key $(cat /proc/bus/input/devices | grep -A10 Keyboard | grep event | awk -F"=" '{print $2}' | grep -Eo "event[0-9]+" | head -n 1) 13 56 29 42 15 56
+	xdotool key "Ctrl+Shift+Tab"
+	# sudo simulate_key $(cat /proc/bus/input/devices | grep -A10 Keyboard | grep event | awk -F"=" '{print $2}' | grep -Eo "event[0-9]+" | head -n 1) 13 56 29 42 15 56
 fi
