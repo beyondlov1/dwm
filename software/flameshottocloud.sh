bf153a0dd3fe73139e380c657fc07014eae0a44d
beyondlov1 <beyondlov1@hotmail.com>
2024-11-21 14:01:35 +0800

    add

--- software/flameshottocloud.sh
+++ software/flameshottocloud.sh
@@ -2,6 +2,7 @@
 # # curl -X POST -F 'file=@/tmp/image.png' http://localhost:8000
 # curl -X POST --data-binary @/tmp/image.png http://localhost:8000
 
-# flameshot gui && xclip -selection clipboard -t image/png -o | curl -X POST --data-binary @- http://192.168.0.112:8000 
-flameshot gui -r | curl -X POST --data-binary @- http://192.168.0.112:8000
+flameshot gui && xclip -selection clipboard -t image/png -o | curl -X POST --data-binary @- http://192.168.0.112:8000 
+# flameshot gui -r | curl -X POST --data-binary @- http://192.168.0.112:8000
+# flameshot gui -r | xclip -selection clipboard -t image/png && xclip -selection clipboard -t image/png -o | curl -X POST --data-binary @- http://192.168.0.112:8000
 
