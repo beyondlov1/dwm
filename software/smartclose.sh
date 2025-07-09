bb0fe0555f6c569607980954bce9734ef9850767
beyond <beyondlov1@hotmail.com>
2025-07-09 20:47:56 +0800

    add

--- software/smartclose.sh
+++ software/smartclose.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 role=$(xprop -id $(xdotool getwindowfocus getwindowgeometry --shell | grep WINDOW | awk -F= '{print $2}') | grep WM_WINDOW_ROLE | awk -F\" '{print $2}')
+wmclass=$(xprop -id $(xdotool getwindowfocus getwindowgeometry --shell | grep WINDOW | awk -F= '{print $2}') | grep WM_CLASS | awk -F\" '{print $2}')
 event=$(cat /proc/bus/input/devices | grep -A10 Keyboard | grep event | awk -F"=" '{print $2}' | grep -Eo "event[0-9]+" | head -n 1)
-if [[ "$role" == "browser" || "$role" == "browser-window" ]]; then
+if [[ "$role" == "browser" || "$role" == "browser-window" || "$wmclass" == "falkon" ]]; then
 	xdotool key "Ctrl+w"
 	# sudo simulate_key $event 2 29 17
 else
@@ -10,4 +11,3 @@ else
 	# sudo simulate_key $event 21 56
 	# sudo simulate_key $event 2 56 62
 fi
-
